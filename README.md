0504f7b898089fd772f9409f2cea1a2a8f1ae08b
rkbhkp <rkbhkp@umsystem.edu>
2021-12-04 12:40:42 -0600

    Added photos and a button for the app

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ install SQLite on vscode extensions
     -to open a db file right click on the file in the file explorer and click open database
     -that opens a tab under the file explorer called "SQLite Explorer" with the db
     -click the arrow or right click and open to open the desired db table
+
+To launch the app cd into my-app and do yarn start
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,19 @@
-import logo from './logo.svg';
+import logo from './pokeball_gray.png';
+import bar from './bar.svg';
 import './App.css';
 
+// 1. Create the button
+var button = document.createElement("button");
+button.innerHTML = "Do Something";
+
+// 2. Append somewhere
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+// 3. Add event handler
+button.addEventListener ("click", function() {
+  alert("did something");
+});
 function App() {
   return (
     <div className="App">
@@ -17,6 +30,7 @@ function App() {
         >
           Learn React
         </a>
+        <img scr={bar} className="App-top" alt="b" />
       </header>
     </div>
   );
