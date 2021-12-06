05d3219c15e97523b1d13e5a05dfab19c33e3fc2
rkbhkp <rkbhkp@umsystem.edu>
2021-12-05 20:12:43 -0600

    Added some UI stuff and a file to run the SQL scripts,

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,37 +1,62 @@
 import logo from './pokeball_gray.png';
 import bar from './bar.svg';
+import ball from './pokeball_red.svg';
 import './App.css';
 
-// 1. Create the button
+var db;
+
+var request = window.indexedDB.open("./../../DB/pokedex.sqlite", 3);
+request.onerror = function(event) {
+  console.log("Why didn't you allow my web app to use IndexedDB?!");
+};
+request.onsuccess = function(event) {
+  db = event.target.result;
+};
+/*// 1. Create the button
 var button = document.createElement("button");
 button.innerHTML = "Do Something";
 
 // 2. Append somewhere
-var body = document.getElementsByTagName("body")[0];
-body.appendChild(button);
+//var body = document.getElementsByTagName("body")[0];
+//body.appendChild(button);
 
 // 3. Add event handler
 button.addEventListener ("click", function() {
   alert("did something");
-});
+});*/
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div >
+      <div className="App">
+        <header className="App-header">
+          <p >
+            NATIONAL POKeDEX
+          </p>
+        </header >
+
+        <div class="fullwidth">
+          <div margin-right="auto">
+          <img src={logo} className="App-logo" alt="logo" />
+          </div>
+        </div>
+
+
+        <content className="App-content">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <img scr={bar} className="App-top" alt="b" />
-      </header>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+            margin-right="auto"
+          >
+            Learn React
+          </a>
+        </content>
+      </div>
     </div>
   );
 }
---
+++ my-app/src/SQLscripts.js
@@ -0,0 +1,16 @@
+const sqlite3 = require('sqlite3').verbose();
+
+//open db
+let db = new sqlite3.Database('./../../DB/pokedex.sqlite');
+
+let sql = 'SELECT identifier Identifier FROM pokemon';
+
+db.all(sql, [], (err, rows) => {
+  if (err) {
+    throw err;
+  }
+  rows.forEach((row) => {
+    console.log(row.Identifier);
+  });
+});
+db.close();
