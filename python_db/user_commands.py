9d796d4efe5ce36a5b09ac3989477a11cfdf8add
rkbhkp <54925741+rkbhkp@users.noreply.github.com>
2021-12-08 17:05:05 -0600

    added a quick menu

--- python_db/user_commands.py
+++ python_db/user_commands.py
@@ -1,12 +1,25 @@
 import sqlite3
 import pandas
 
+
+
 conn = sqlite3.connect('python_db/pokedex_db.sqlite')
 c = conn.cursor()
 
-c.execute('''
-        UPDATE Pokemon
-        SET m2 = 'Acid'
-        where id = 1
-        ''')
+u_in = 'test'
+while(u_in != 'q'):
+    print("----------Pokedex----------")
+    print("\tPlease select one of the options below")
+    print('\tAdd Move: m')
+    print("\tquit: q")
+    u_in = input('\tselection: ')
+    if(u_in == 'm'):
+        c.execute('''
+                UPDATE Pokemon
+                SET m2 = 'Absorb'
+                where id = 1
+                ''')
+    else: 
+        print('\n\n\tPlease enter a valid input')
+        
 conn.commit()
