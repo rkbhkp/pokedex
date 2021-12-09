2841da554d45823bda8f1c7e531de2e88ae52eec
rkbhkp <54925741+rkbhkp@users.noreply.github.com>
2021-12-08 19:30:03 -0600

    added some error catches and add move set

--- python_db/user_commands.py
+++ python_db/user_commands.py
@@ -18,41 +18,56 @@ while(u_in != 'q'):
     print("\tquit: q")
     u_in = input('\n\tselection: ')
     if(u_in == 'm'):
-        # add move to pokemon
-        c.execute('''
-                UPDATE Pokemon
-                SET m2 = 'Absorb'
-                where id = 1
-                ''')
-    elif(u_in == 'u'):
-        # adds a user named test to the user table 
-        uname = input('\n\t Enter the username: ')
-        if has_value(c, 'User', 'user_id', uname):
-            print('There is already a user with the name %s\n' % uname)
-        else:
+        try:
+            # add move to pokemon
+            p_id = input('\n\tWhat is the id of the pokemon you want to add: ')
+            m_name = input('\n\tWhat name of the move you want to add to the pokemon: ')
+            m_slot = input('\n\tWhat move slot do you want to add the move to (1-4): ')
+            while(int(m_slot) < 1 or int(m_slot) > 4):
+                m_slot = input('\n\tPlease input a valid team slot (1-4): ')
+    #probably still needs to validate that the name of the move is a valid move
             c.execute('''
-                    INSERT INTO User (user_id)
-                    SELECT '%s'
-                        WHERE NOT EXISTS (SELECT 1 FROM User WHERE user_id = '%s')
-                    '''%(uname, uname))
-            print('\n\tUser added')
+                    UPDATE Pokemon
+                    SET %s = '%s'
+                    where id = %s
+                    '''%('m'+m_slot, m_name, p_id))
+        except:
+            print("\n\tError: Unable to add move to list")
+    elif(u_in == 'u'):
+        try:
+            # adds a user named test to the user table 
+            uname = input('\n\t Enter the username: ')
+            if has_value(c, 'User', 'user_id', uname):
+                print('There is already a user with the name %s\n' % uname)
+            else:
+                c.execute('''
+                        INSERT INTO User (user_id)
+                        SELECT '%s'
+                            WHERE NOT EXISTS (SELECT 1 FROM User WHERE user_id = '%s')
+                        '''%(uname, uname))
+                print('\n\tUser added')
+        except: 
+            print("\n\n\tError: Unable to add user")
 
     elif(u_in == 'p'):
-        # adds the pokemon to the users team, still need to figure out how to input which pokemon they are adding 
-        u_select = input('\n\tWhat user do you want to add the pokemon to: ')
-        p_slot = input('\n\tWhat team slot do you want to add the pokemon to (1-6): ')
-        while(int(p_slot) < 1 or int(p_slot) > 6):
-            p_slot = input('\n\tPlease input a valid team slot (1-6): ')
+        try:
+            # adds the pokemon to the users team, still need to figure out how to input which pokemon they are adding 
+            u_select = input('\n\tWhat user do you want to add the pokemon to: ')
+            p_slot = input('\n\tWhat team slot do you want to add the pokemon to (1-6): ')
+            while(int(p_slot) < 1 or int(p_slot) > 6):
+                p_slot = input('\n\tPlease input a valid team slot (1-6): ')
 
-        p_name = input('\n\tWhat is the id of the pokemon you want to add: ')
-       
-#currently it just accepts an ID of the pokemon idealy we would allow this to accept any name that is in the list of pokemon,
-# can probably accomplish this by something similar to how we validate if a user is already in the list
-        c.execute('''
-                UPDATE User
-                Set %s = %s
-                WHERE user_id = '%s'
-                '''%('p'+p_slot, p_name, u_select))
+            p_name = input('\n\tWhat is the name of the pokemon you want to add: ')
+        
+    #currently it just accepts an name of the pokemon idealy we would allow this to accept any name that is in the list of pokemon,
+    # can probably accomplish this by something similar to how we validate if a user is already in the list
+            c.execute('''
+                    UPDATE User
+                    Set %s = '%s'
+                    WHERE user_id = '%s'
+                    '''%('p'+p_slot, p_name, u_select))
+        except: 
+            print('\n\tError: Unable to add pokemon to team')
         
     else: 
         print('\n\n\tPlease enter a valid input')
